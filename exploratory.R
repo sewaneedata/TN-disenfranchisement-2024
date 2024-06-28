bb61da22e238177fd51fc25e379776eb72e2695b
Kayla Ahrndt <kayla.ahrndt@gmail.com>
2024-06-28 10:00:43 -0500

    Updating with comments

--- exploratory.R
+++ exploratory.R
@@ -14,18 +14,28 @@ library(readr)
 census <- read_csv('census.csv')
 View(census)
 
+# create df with census tract data from acs
+# rename UniqueIDs
 first_acs <- get_acs(geography = "tract",
                      state = "TN",
                      #county = "Hamilton",
                      variables = c(
+                       #poverty income
                        poverty_income = "B23024_002",
+                       #white 
                        white = "B02001_002", #"B02003_003", #detailed race codes start here
+                       #african american
                        afr_amr = "B02001_003", #"B02003_004",
+                       #indigenous
                        nativeamr = "B02001_004", #"B02003_005",
+                       #asian
                        asian = "B02001_005", #"B02003_006",
+                       #pacific islander
                        pac_isl = "B02001_006", #"B02003_007",
+                       #other race
                        otherrace = "B02001_007", #"B02003_008",
-                       lessthan10k = "B25122_002", #household income last 12 months start here
+                       #household income over the last 12 months follows
+                       lessthan10k = "B25122_002",
                        btwn10kand19999 = "B25122_019",
                        btwn20kand34999 = "B25122_036",
                        btwn35kand49999 = "B25122_053",
