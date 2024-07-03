d891fb0cb2b2a24b2ee363fe95f40fb1c68270c7
Kayla Ahrndt <kayla.ahrndt@gmail.com>
2024-07-03 16:55:23 -0500

    Cleaning up old unsuccessful attempts

--- exploratory.R
+++ exploratory.R
@@ -96,12 +96,6 @@ View(census_votes)
 # census_votes <- census_votes %>%
 #   pivot_longer(cols = starts_with('btwn'),
 #                values_to = 'income')
-#   # mutate(income = btwn10kand19999 +
-#   #           btwn20kand34999 +
-#   #           btwn35kand49999 +
-#   #           btwn50kand74999 +
-#   #           btwn75kand99999 +
-#   #           over100k)
 # View(census_votes)
         #PROBLEMS:
           #turned btwn columns into new column, which means they're gone
@@ -118,38 +112,8 @@ View(census_votes)
 #   arrange(income)
 # View(arranged_census_votes)
 
-# # in order to make the next heat map sing census_votes, you must
-# # determine the maximum length of the columns
-# target_length <- max(sapply(census_votes, length))
-# target_length
-# 
-# # function to pad columns with NA to match target length
-# pad_column <- function(income, target_length) {
-#   length(income) <- target_length
-#   return(income)
-# }
-# 
-# # apply the padding function to each column in the data frame
-# census_votes_padded <- as.data.frame(lapply(census_votes, pad_column, target_length = target_length))
-# 
-# # replace NA values with appropriate value (NA) if needed
-# census_votes_padded[is.na(census_votes_padded)] <- NA
-# # not NA, 0
-# #census_votes_padded[is.na(census_votes_padded)] <- 0
-# 
-# # view the padded data frame
-# print(census_votes_padded)
-# View(census_votes_padded)
-
 # convert dataframe into a sf type object
 census_votes <- st_sf(census_votes)
-
-# # heat map for income
-# tmap_mode("plot")
-# tm_shape(census_votes) +
-#   tm_polygons(alpha = 0.8, col = c('income'), id = "NAME") +
-#   # make several layered maps that you can toggle between
-#   tm_facets(as.layers = TRUE) 
 
 #heat map for income
 tmap_mode("plot")
